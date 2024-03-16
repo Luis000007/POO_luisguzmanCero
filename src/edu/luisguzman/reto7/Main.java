d9817508c35ec60ad130020afeb48bb7d777c97d
eg787 <luisemmanuelguzman20@gmail.com>
2024-03-16 13:36:53 -0600

    Retos de la clase programacion orientada a objetos

---
+++ src/edu/luisguzman/reto7/Main.java
@@ -0,0 +1,13 @@
+package edu.luisguzman.reto7;
+
+import edu.luisguzman.reto7.process.Calculos;
+import edu.luisguzman.reto7.vasos_de_cafe.VasosDeCafe;
+
+public class Main {
+    public static void main (String[] args) {
+
+        VasosDeCafe.main(args);
+        Calculos.main(args);
+
+    }
+}
